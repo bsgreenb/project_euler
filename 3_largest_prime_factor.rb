117f6615f54710d47e1f92887411dc6981a2217d
Ben Greenberg <bsgreenb@gmail.com>
2017-10-01 20:57:35 -0400

    Begin the prime problem

--- 3_largest_prime_factor.rb
+++ 3_largest_prime_factor.rb
@@ -2,6 +2,25 @@
 #
 # What is the largest prime factor of the number 600851475143 ?
 
-def largest_prime_factors(n)
+#thoughts on solving
+# Prime numbers are numbers that aren't made of other prime numbers.  We can go up to the sqrt of n
 
+#CONTINYA here on fixing the prime finder.  It's not tripping when it should be.
+def largest_prime_factors(n)
+  primes = []
+  i = 2
+  while i < Math.sqrt(n)
+    prime = true
+    primes.each do |prime|
+      if i % prime == 0
+        prime = false
+        break
+      end
+    end
+    primes.push(i) if prime
+    i = i + 1
+  end
+  puts primes.inspect
 end
+
+puts largest_prime_factors(13195) #5, 7, 13 and 29
