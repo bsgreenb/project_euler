1189dda05d191c268a3ec5b7bcbc9da1c288ea3a
Ben Greenberg <bsgreenb@gmail.com>
2017-10-01 20:10:43 -0400

    Finished problem 2 so start problem 3

--- 1_multiples_of_3_and_5.rb
+++ 1_multiples_of_3_and_5.rb
@@ -3,3 +3,18 @@
 # The sum of these multiples is 23.
 
 # Find the sum of all the multiples of 3 or 5 below 1000.
+
+def sum_of_multiples(up_to = 1000)
+  sum = 0
+  i = 1
+  while i < up_to
+    if i % 3 == 0 || i % 5 == 0
+      sum = sum + i
+    end
+    i = i + 1
+  end
+
+  sum
+end
+
+puts sum_of_multiples()
